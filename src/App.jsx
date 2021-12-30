bc0f2fc3161db166b2a3d479680c29c340615370
Dougueta <odouglopes@gmail.com>
2021-12-30 17:09:13 -0300

    Adding public url

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ import './App.css'
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <PageBase>
         {/* <Switch> */}
         <Route
